fdc57796d5d4465a0ae5fb2c231c93a7e7fe7315
Joshua <josh.aremou229@gmail.com>
2025-05-10 13:01:34 +0200

    Etape 9 Automatisation Server pour toutes les villes

--- server.R
+++ server.R
@@ -1,9 +1,14 @@
 # Serveur
 server <- function(input, output, session) {
   
-  mod_city_analysis_server(id = "nyc", 
-                           data = newyork, 
-                           neighbourhood_column = "neighbourhood", 
-                           price_column = "price")
+  walk(
+    cities_info,
+    ~ mod_city_analysis_server(
+      id = .x$input_id_prefix,
+      data = .x$data,
+      neighbourhood_column = .x$neighbourhood_column,
+      price_column = .x$price_column
+    )
+  )
   
 }
