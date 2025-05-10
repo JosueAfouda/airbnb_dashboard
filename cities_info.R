053fa9654ddaaaac29a94c5a582b830957bd572d
Joshua <josh.aremou229@gmail.com>
2025-05-10 13:12:25 +0200

    Etape10 Ajout de la ville Amsterdam

--- cities_info.R
+++ cities_info.R
@@ -21,5 +21,18 @@ cities_info <- list(
     map_output_id = "sf_map",
     avg_price_output_id = "sf_quartier_avg_price",
     price_chart_output_id = "sf_price_chart"
+  ),
+  
+  # Ajout de la ville Amsterdam
+  list(
+    city_name = "Amsterdam",
+    input_id_prefix = "ams",
+    data = amsterdam,
+    neighbourhood_column = "neighbourhood",
+    neighbourhood_choices = unique(amsterdam$neighbourhood),
+    price_column = "price",
+    map_output_id = "ams_map",
+    avg_price_output_id = "ams_quartier_avg_price",
+    price_chart_output_id = "ams_price_chart"
   )
 )
