dfffcecac571cafbb1e1e8ed4a424ff7f7532e08
Joshua <josh.aremou229@gmail.com>
2025-05-10 08:12:21 +0200

    Etape 2 data analysis et code UI pour une ville

--- ui.R
+++ ui.R
@@ -9,8 +9,50 @@ header <- dashboardHeader(
 sidebar <- dashboardSidebar()
 
 # Body
+neighbourhood_choices <- unique(newyork$neighbourhood)
+city_name <- "NewYork"
 body <- dashboardBody(
-  
+  tabPanel(
+    title = city_name,
+    icon = icon("city"),
+    fluidRow(
+      box(
+        width = 12, 
+        selectInput("quartier", 
+                    "Choisis un quartier", 
+                    choices = neighbourhood_choices)
+      ),
+      box(
+        width = 7, 
+        title = "Emplacements des logements Airbnb",
+        status = "primary", 
+        solidHeader = TRUE, 
+        leafletOutput("map")
+      ),
+      box(
+        width = 5,
+        fluidRow(
+          box(
+            width = 12, 
+            title = "Prix moyen ($) d’un Airbnb dans ce quartier",
+            status = "primary", 
+            solidHeader = TRUE, 
+            valueBoxOutput("avg_price", width = 12)
+          )
+        ),
+        br(),
+        fluidRow(
+          box(
+            width = 12, 
+            title = "Prix moyen ($) d’un Airbnb par type de logement",
+            status = "primary", 
+            solidHeader = TRUE, 
+            plotlyOutput("price_chart")
+          )
+        )
+      )
+    )
+  )
 )
 
 # User Interface
