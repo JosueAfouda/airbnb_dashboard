5ca9fa49b4caca2c7e8379ffbf58e4b1f62981d7
Joshua <josh.aremou229@gmail.com>
2025-05-10 07:57:05 +0200

    Etape 1 initiation de l'app

--- server.R
+++ server.R
@@ -1,14 +1,4 @@
-# server
+# Serveur
 server <- function(input, output, session) {
-  
-  purrr::walk(
-    city_info_server(),
-    ~ mod_city_analysis_server(
-      id = .x$input_id_prefix,
-      data = .x$data,
-      neighbourhood_column = .x$neighbourhood_column,
-      price_column = .x$price_column
-    )
-  )
   
 }
